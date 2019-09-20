54aaaaabc5388474b5c72d499a2d0bbcc840cba1
System Administrator <admin@example.com>
2019-09-20 02:38:46 -0400

    Changes by 'System Administrator' on 'devops_demo_one – 1.0.0' -- Build #1

---
+++ sn/devops_demo_one/Fix Script/active_true/devops_demo 1.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : devops_demo_one
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-09-20 06:31:52
+ * Created By  : admin
+ * Updated On  : 2019-09-20 06:31:52
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=e99f1e74db40001011c27a61399619d9
+ */
+gs.log("Hi this is a demo script")
